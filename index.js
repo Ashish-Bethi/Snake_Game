07be33f307583363cf3034c4e9792f7242345fc6
Ashish-Bethi <ashish.bethi@gmail.com>
2024-12-15 18:03:25 +0530

    added script.js and basic variables

---
+++ index.js
@@ -0,0 +1,18 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "green";
+const snakeColor = "purple";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+//defining snake
